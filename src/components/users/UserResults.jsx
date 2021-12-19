ea04e2dcaec6201f98815dfdeb801c8dac260519
KhaledAlabssi <khaled.absi5@gmail.com>
2021-12-19 16:39:42 +0100

    add context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,20 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useContext} from 'react'
+import GithubContext from '../../context/github/GithubContext'
 import Spinner from '../layers/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const {users, loading, fetchUsers} = useContext(GithubContext)
     useEffect(() => {
         fetchUsers()
         
     }, [])
 
-    const fetchUsers = async () => {
-        const response = await fetch('https://api.github.com/users')
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
-    }
+   
     return (<>
         {loading ? (
             <h1><Spinner /></h1>
@@ -22,7 +18,7 @@ function UserResults() {
         ) : (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
             {users.map(user => (
-                <h2>{user.login}</h2>
+                <UserItem key={user.id} user={user}/>
             ))}
             
         </div>
