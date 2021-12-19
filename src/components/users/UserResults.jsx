d3f87518c3e80bbbacc5de27950a59a9c8aaf155
KhaledAlabssi <khaled.absi5@gmail.com>
2021-12-19 10:34:02 +0100

    users component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react'
+
+function UserResults() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        fetchUsers()
+        
+    }, [])
+
+    const fetchUsers = async () => {
+        const response = await fetch('https://api.github.com/users')
+        const data = await response.json()
+        setUsers(data)
+        setLoading(false)
+    }
+    return (<>
+        {loading ? (
+            <h1>Loading...</h1>
+
+        ) : (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+            {users.map(user => (
+                <h2>{user.login}</h2>
+            ))}
+            
+        </div>
+
+        )}
+        </>
+        
+    )
+}
+
+export default UserResults
